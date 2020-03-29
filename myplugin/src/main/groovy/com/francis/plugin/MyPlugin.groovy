c54d8ee73a314d0dffce01640279651ae3d0e838
HuangMingFrancis <franciszhen@163.com>
2020-03-29 23:00:19 +0800

    自定义gradle插件

---
+++ myplugin/src/main/groovy/com/francis/plugin/MyPlugin.groovy
@@ -0,0 +1,23 @@
+package com.francis.plugin
+
+import org.gradle.api.Plugin
+
+import org.gradle.api.Project
+
+public class MyPlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+
+        def log = project.logger
+
+        log.error "========================"
+
+        log.error "完整的MyPlugin，开始修改Class!"
+
+        log.error "========================"
+
+        println("---------- plugin -------------")
+
+    }
+
+}
